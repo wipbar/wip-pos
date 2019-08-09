2c0b93af4458304ec206c7ad3afb23c891a2e1e5
klarstrup <jonas@klarstrup.dk>
2019-08-09 15:58:02 +0200

    keep old sales products maybe

--- server/main.js
+++ server/main.js
@@ -9,6 +9,27 @@ Meteor.publish("sales", () => Sales.find());
 Meteor.publish("stocks", () => Stocks.find());
 
 Accounts.config({ forbidClientAccountCreation: true });
+/*
+const products = Products.find({ removedAt: { $exists: false } }).fetch();
+const sales = Sales.find({}).fetch();
+sales.forEach(sale => {
+  Sales.update(
+    { _id: sale._id },
+    {
+      $set: {
+        products: sale.products.map(sP => {
+          const currentProduct = products.find(cP => cP._id === sP._id);
+          return {
+            ...sP, 
+            brandName: currentProduct ? currentProduct.brandName : sP.brandName,
+            name: currentProduct ? currentProduct.name : sP.name,
+          };
+        }),
+      },
+    },
+  );
+});
+*/
 
 if (!Meteor.users.find({}).fetch().length)
   Accounts.createUser({
