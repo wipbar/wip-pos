a1b6c38e596915e5050080142181d42911630fef
klarstrup <jonas@klarstrup.dk>
2019-08-11 15:26:43 +0200

    oij

--- ui/PageMenu.js
+++ ui/PageMenu.js
@@ -40,81 +40,83 @@ export default function PageMenu() {
         max-height: 100%;
       `}
     >
-      {productsGroupedByTags.map(([tags, products], i) => {
-        return (
-          <div
-            key={tags}
-            className={css`
-              -webkit-column-break-inside: avoid;
-              page-break-inside: avoid;
-              break-inside: avoid;
-            `}
-          >
-            <h2
-              className={
-                i == 0
-                  ? css`
-                      margin-top: 0;
-                    `
-                  : undefined
-              }
-            >
-              {tags.split(",").join(", ")}
-            </h2>
-            <ul
+      {productsGroupedByTags
+        .sort((a, b) => b[1].length - a[1].length)
+        .map(([tags, products], i) => {
+          return (
+            <div
+              key={tags}
               className={css`
-                margin: 0;
-                padding: 0;
-                list-style: none;
+                -webkit-column-break-inside: avoid;
+                page-break-inside: avoid;
+                break-inside: avoid;
               `}
             >
-              {products.map(product => (
-                <li
-                  key={product._id}
-                  className={css`
-                    margin: 0;
-                    padding: 4px 6px;
-                    display: flex;
-                    background: rgba(255, 255, 255, 0.1);
-                    margin-bottom: 4px;
-                    align-items: center;
-                  `}
-                >
-                  <div
+              <h2
+                className={
+                  i == 0
+                    ? css`
+                        margin-top: 0;
+                      `
+                    : undefined
+                }
+              >
+                {tags.split(",").join(", ")}
+              </h2>
+              <ul
+                className={css`
+                  margin: 0;
+                  padding: 0;
+                  list-style: none;
+                `}
+              >
+                {products.map(product => (
+                  <li
+                    key={product._id}
                     className={css`
-                      flex: 1;
-                    `}
-                  >
-                    <small>
-                      <small>{product.brandName}</small>
-                    </small>
-                    <br />
-                    {product.name}
-                  </div>
-                  <div
-                    className={css`
-                      margin-left: 5px;
-                      font-size: 1.1em;
-                      text-align: center;
+                      margin: 0;
+                      padding: 4px 6px;
+                      display: flex;
+                      background: rgba(255, 255, 255, 0.1);
+                      margin-bottom: 4px;
+                      align-items: center;
                     `}
                   >
                     <div
                       className={css`
-                        margin-bottom: -12px;
+                        flex: 1;
                       `}
                     >
-                      <b>{product.salePrice}</b>
+                      <small>
+                        <small>{product.brandName}</small>
+                      </small>
+                      <br />
+                      {product.name}
                     </div>
-                    <small>
-                      <small>HAX</small>
-                    </small>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        );
-      })}
+                    <div
+                      className={css`
+                        margin-left: 5px;
+                        font-size: 1.1em;
+                        text-align: center;
+                      `}
+                    >
+                      <div
+                        className={css`
+                          margin-bottom: -12px;
+                        `}
+                      >
+                        <b>{product.salePrice}</b>
+                      </div>
+                      <small>
+                        <small>HAX</small>
+                      </small>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
     </div>
   );
 }
