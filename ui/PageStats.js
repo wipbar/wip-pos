b6671e6116a98b565274f54cb9bdf026ddae13cc
klarstrup <jonas@klarstrup.dk>
2019-08-11 02:43:17 +0200

    primimiate graph hope it doesn't crash the ipad

--- ui/PageStats.js
+++ ui/PageStats.js
@@ -1,8 +1,17 @@
-import React from "react";
+import {
+  addHours,
+  endOfHour,
+  getHours,
+  isAfter,
+  isBefore,
+  startOfHour,
+} from "date-fns";
+import { css } from "emotion";
+import React, { useMemo } from "react";
+import Products from "../api/products";
+import Sales from "../api/sales";
 import useSubscription from "../hooks/useSubscription";
 import useTracker from "../hooks/useTracker";
-import Sales from "../api/sales";
-import Products from "../api/products";
 
 export default function PageStats() {
   useSubscription("sales");
@@ -11,16 +20,85 @@ export default function PageStats() {
   const products = useTracker(() =>
     Products.find({ removedAt: { $exists: false } }).fetch(),
   );
-  const mostSold = Object.entries(
-    sales.reduce((m, sale) => {
-      sale.products.forEach(product => {
-        m[product._id] = m[product._id] ? m[product._id] + 1 : 1;
-      });
-      return m;
-    }, {}),
-  ).sort(([, a], [, b]) => b - a);
+  const [firstSale] = useMemo(
+    () => (sales ? [...sales].sort((a, b) => a.timestamp - b.timestamp) : []),
+    [sales],
+  );
+  const allHours = useMemo(() => {
+    let hours = [];
+    if (firstSale) {
+      const from = startOfHour(firstSale.timestamp);
+      const to = endOfHour(new Date());
+      for (let i = from; isBefore(i, to); i = addHours(i, 1)) {
+        hours.push(i);
+      }
+    }
+    return hours;
+  }, [firstSale, sales]);
+  console.log(allHours);
+  const salesByHour = allHours.map(hour => {
+    const hourEnd = endOfHour(hour);
+    return [
+      hour,
+      sales.filter(
+        sale =>
+          isAfter(sale.timestamp, hour) && isBefore(sale.timestamp, hourEnd),
+      ),
+    ];
+  });
+  console.log(salesByHour);
+
+  const mostSold = useMemo(
+    () =>
+      Object.entries(
+        sales.reduce((m, sale) => {
+          sale.products.forEach(product => {
+            m[product._id] = m[product._id] ? m[product._id] + 1 : 1;
+          });
+          return m;
+        }, {}),
+      ).sort(([, a], [, b]) => b - a),
+    [sales],
+  );
   return (
     <>
+      <div
+        className={css`
+          display: flex;
+          max-width: 100%;
+          height: ${1 +
+            salesByHour.reduce((m, [, hourSales]) => {
+              const total = hourSales.reduce(
+                (m, hourSale) => hourSale.products.length + m,
+                0,
+              );
+              if (total > m) m = total;
+              return m;
+            }, 0)}em;
+          align-items: flex-end;
+        `}
+      >
+        {salesByHour.map(([hour, hourSales]) => {
+          return (
+            <div
+              key={hour}
+              className={css`
+                flex: 1;
+                text-align: center;
+                font-size: 0.8em;
+                background-color: red;
+                height: ${1 +
+                  hourSales.reduce(
+                    (m, hourSale) => hourSale.products.length + m,
+                    0,
+                  )}em;
+              `}
+            >
+              {getHours(hour)}
+            </div>
+          );
+        })}
+      </div>
       Most sold:
       <ul>
         {mostSold.map(([productId, count]) => {
