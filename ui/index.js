3e5596d70a729e46ec12a53b35404921e0d80416
klarstrup <jonas@klarstrup.dk>
2020-08-11 18:04:49 +0200

    menu ONLY

--- ui/index.js
+++ ui/index.js
@@ -69,6 +69,7 @@ export default function UI() {
           background: rgba(255, 255, 255, 0.2);
           border-bottom: 2px solid rgba(255, 255, 255, 0.3);
         `}
+        hidden={pageSlug === "menu" && !user}
       >
         <nav
           className={css`
