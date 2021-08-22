97d7f6fb7d881adb5d66aac3da6f9c24e97fc9ca
klarstrup <jonas@klarstrup.dk>
2021-08-22 16:43:44 +0200

    shitty trend line

--- components/CampByCamp.js
+++ components/CampByCamp.js
@@ -5,6 +5,7 @@ import {
   Legend,
   Line,
   LineChart,
+  ReferenceDot,
   ReferenceLine,
   ResponsiveContainer,
   Tooltip,
@@ -14,6 +15,50 @@ import {
 import Camps from "../api/camps";
 import Sales from "../api/sales";
 import useMongoFetch from "../hooks/useMongoFetch";
+
+function getAvg(arr) {
+  const total = arr.reduce((acc, c) => acc + c, 0);
+  return total / arr.length;
+}
+
+function getSum(arr) {
+  return arr.reduce((acc, c) => acc + c, 0);
+}
+
+function createTrend(data, xKey, yKey) {
+  const xData = data.map((value) => value[xKey]);
+  const yData = data.map((value) => value[yKey]);
+
+  // average of X values and Y values
+  const xMean = getAvg(xData);
+  const yMean = getAvg(yData);
+
+  // Subtract X or Y mean from corresponding axis value
+  const xMinusxMean = xData.map((val) => val - xMean);
+  const yMinusyMean = yData.map((val) => val - yMean);
+
+  const xMinusxMeanSq = xMinusxMean.map((val) => Math.pow(val, 2));
+
+  const xy = [];
+  for (let x = 0; x < data.length; x++) {
+    xy.push(xMinusxMean[x] * yMinusyMean[x]);
+  }
+
+  // const xy = xMinusxMean.map((val, index) => val * yMinusyMean[index]);
+
+  const xySum = getSum(xy);
+
+  // b1 is the slope
+  const b1 = xySum / getSum(xMinusxMeanSq);
+  // b0 is the start of the slope on the Y axis
+  const b0 = yMean - b1 * xMean;
+
+  return {
+    slope: b1,
+    yStart: b0,
+    calcY: (x) => b0 + b1 * x,
+  };
+}
 
 export default function CampByCamp() {
   const { data: camps, loading: campsLoading } = useMongoFetch(Camps, []);
@@ -83,11 +128,48 @@ export default function CampByCamp() {
     sales,
   ]);
   if (campsLoading || salesLoading) return "Loading...";
+  let prev = 0;
+  const weights = data.map((data) => (prev = data["bornhack-2021"] || prev));
+  const yMax = Math.max(...weights);
+  const yMin = Math.min(...weights);
+  const timestamps = data.map((data) => data.hour);
+  const xMax = Math.max(...timestamps);
+  const nowHour = Math.max(
+    ...data.filter((d) => d["bornhack-2021"]).map((data) => data.hour),
+  );
+  const xMin = Math.min(...timestamps);
+
+  const trendData = (() => {
+    const trend = createTrend(
+      data.filter((d) => d["bornhack-2021"]),
+      "hour",
+      "bornhack-2021",
+    );
+
+    return [
+      {
+        "bornhack-2021-trend": data.find(
+          (d) => d["bornhack-2021"] && d.hour === nowHour,
+        )["bornhack-2021"],
+        hour: nowHour,
+      },
+      {
+        "bornhack-2021-trend": trend.calcY(Math.min(xMax, nowHour + 36)),
+        hour: Math.min(xMax, nowHour + 36),
+      },
+    ];
+  })();
 
   return (
     <ResponsiveContainer width={"100%"} height={350}>
       <LineChart
-        data={data}
+        data={data.map((d) =>
+          d.hour === trendData[0].hour
+            ? { ...d, ...trendData[0] }
+            : d.hour === trendData[1].hour
+            ? { ...d, ...trendData[1] }
+            : d,
+        )}
         margin={{ top: 0, right: 20, left: 40, bottom: 0 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
@@ -127,13 +209,26 @@ export default function CampByCamp() {
             }}
           />
         )}
+        <ReferenceDot
+          y={yMax}
+          x={nowHour}
+          key="Now"
+          label={{
+            value: currentCamp?.start?.getFullYear(),
+            position: "right",
+            style: { fill: currentCamp?.color },
+          }}
+          fill={currentCamp?.color}
+          r={4}
+          stroke={currentCamp?.color}
+        />
         {camps.map((camp, i) =>
           i < camps.length - 1 ? (
             <ReferenceLine
               y={campTotals[camp.slug]}
               key={camp.slug + "-ReferenceLine"}
               label={{
-                value: "Max " + camp.name,
+                value: "Max " + camp.start?.getFullYear(),
                 position: "insideTop",
                 style: { fill: camp.color },
               }}
@@ -155,6 +250,15 @@ export default function CampByCamp() {
             connectNulls
           />
         ))}
+        <Line
+          key={currentCamp.slug + "-trend"}
+          dataKey={currentCamp.slug + "-trend"}
+          stroke={currentCamp.color}
+          strokeWidth={2}
+          strokeDasharray="4 2"
+          dot={false}
+          connectNulls
+        />
       </LineChart>
     </ResponsiveContainer>
   );
