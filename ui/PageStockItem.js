3a029274f4408fa9ff9b8c4b9ab329fe9cd1ddbd
klarstrup <jonas@klarstrup.dk>
2021-08-22 16:07:44 +0200

    more taps

--- ui/PageStockItem.js
+++ ui/PageStockItem.js
@@ -177,6 +177,8 @@ export default function PageStockItem({ onCancel, product }) {
           <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
         </select>
       </Label>
       <Label label="Tags">
