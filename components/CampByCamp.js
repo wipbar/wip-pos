3f05c175a7ef4d13326ba6befbb2a27375b19c51
klarstrup <jonas@klarstrup.dk>
2021-08-23 15:45:20 +0200

    campbycamp fix y axis format

--- components/CampByCamp.js
+++ components/CampByCamp.js
@@ -179,6 +179,7 @@ export default function CampByCamp() {
         />
         <YAxis
           domain={["dataMin", "dataMax"]}
+          tickFormatter={(amount) => ~~amount}
           label={{
             value: "Revenue (HAX)",
             angle: -90,
