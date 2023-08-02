b3a65c2d02638767cabb4c5509147b3b5d52015f
Florian Klink <flokli@flokli.de>
2023-08-02 17:53:00 +0200

    update application-specific metrics
    Change location_product_sales from a gauge to a counter, rename to
    pos_sales_count

--- server/metrics.ts
+++ server/metrics.ts
@@ -4,40 +4,32 @@ import { WebApp } from "meteor/webapp";
 import Locations from "../api/locations";
 import Products from "../api/products";
 import Sales from "../api/sales";
-import * as client from "../vendor/prom-client";
+import * as promClient from "../vendor/prom-client";
 
-new client.Gauge({
-  name: "location_product_sales",
-  help: "Sale by product",
-  labelNames: ["locationName", "brandName", "productName"],
-  collect() {
-    const locations = Locations.find().fetch();
-    locations.forEach((location) => {
-      const locationSales = Sales.find({ locationId: location._id }).fetch();
-      Products.find().forEach(({ brandName, name: productName, _id }) => {
-        const count = sumBy(
-          locationSales,
-          (sale) =>
-            sale.products.filter((saleProduct) => saleProduct._id == _id)
-              .length,
-        );
-        // if (count)
-        this.labels(location.name, brandName || "", productName).set(count);
-      });
-    });
-  },
+// define application specific metrics
+export const SalesCounter = new promClient.Counter({
+    name: "pos_sales_count",
+    help: "Counter for sales of products",
+    labelNames: ["location", "brand", "product"]
 });
-client.collectDefaultMetrics();
 
-WebApp.connectHandlers.use("/metrics", (_req, res) =>
-  Fiber(async () => {
-    try {
-      res.setHeader("content-type", "text/plain; version=0.0.4; charset=utf-8");
-      return res.end(await client.register.metrics());
-    } catch (error) {
-      console.error(error);
-      res.setHeader("content-type", "application/json");
-      return res.end(JSON.stringify(error));
-    }
-  }).run(),
-);
+// respond to /metrics and serve metrics from there.
+export function registerMetricsHandler() {
+  // enable default metrics collection
+  promClient.collectDefaultMetrics();
+
+  // register the URL handler
+  WebApp.connectHandlers.use("/metrics", (_req, res) =>
+    Fiber(async () => {
+      try {
+        res.setHeader("content-type", "text/plain; version=0.0.4; charset=utf-8");
+        return res.end(await promClient.register.metrics());
+      } catch (error) {
+        console.error(error);
+        res.setHeader("content-type", "application/json");
+        return res.end(JSON.stringify(error));
+      }
+    }).run(),
+  );
+}
+
