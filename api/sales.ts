72363732d9ac26dad4118c104aa460b28996489c
klarstrup <io@klarstrup.dk>
2024-07-17 22:54:18 +0200

    refactor: Update sales data calculation interval to 100 seconds

--- api/sales.ts
+++ api/sales.ts
@@ -210,7 +210,7 @@ if (Meteor.isServer) {
     setInterval(async () => {
       statsCampByCamp = await calculateCampByCampStats();
       statsSalesSankey = await calculateSalesSankeyData();
-    }, 30_000);
+    }, 100_000);
   });
 }
 
