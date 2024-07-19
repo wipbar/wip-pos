30ae743e4816b8568a757a7f14f01eabdc455c36
klarstrup <io@klarstrup.dk>
2024-07-19 14:51:46 +0200

    stop /sales for now

--- server/sales.ts
+++ server/sales.ts
@@ -2,19 +2,21 @@ import Fiber from "fibers";
 import { WebApp } from "meteor/webapp";
 import Sales from "../api/sales";
 
-WebApp.connectHandlers.use("/sales", (_req, res) =>
-  Fiber(async () => {
-    try {
-      res.setHeader("content-type", "application/json; charset=utf-8");
-      return res.end(
-        JSON.stringify(
-          Sales.find({}, { fields: { "products.shopPrices": 0 } }).fetch(),
-        ),
-      );
-    } catch (error) {
-      console.error(error);
-      res.setHeader("content-type", "application/json");
-      return res.end(JSON.stringify(error));
-    }
-  }).run(),
-);
+if (Number(false)) {
+  WebApp.connectHandlers.use("/sales", (_req, res) =>
+    Fiber(async () => {
+      try {
+        res.setHeader("content-type", "application/json; charset=utf-8");
+        return res.end(
+          JSON.stringify(
+            Sales.find({}, { fields: { "products.shopPrices": 0 } }).fetch(),
+          ),
+        );
+      } catch (error) {
+        console.error(error);
+        res.setHeader("content-type", "application/json");
+        return res.end(JSON.stringify(error));
+      }
+    }).run(),
+  );
+}
