84f774fe4623b01b92f73d2862b8a82a569c523b
klarstrup <io@klarstrup.dk>
2025-07-17 21:15:55 +0200

    adjust column widths in PageMenu for improved layout

--- ui/PageMenu.tsx
+++ ui/PageMenu.tsx
@@ -195,9 +195,9 @@ export default function PageMenu() {
       className={css`
         font-size: 0.8em;
         padding: 0.5em;
-        column-width: 15em;
+        column-width: 13em;
         @media (min-width: 1400px) {
-          column-width: 16vw;
+          column-width: 15vw;
         }
         column-fill: balance;
         column-gap: 0.5em;
