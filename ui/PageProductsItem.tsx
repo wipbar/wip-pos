8aa6161320f8a27e93dc687a5e84bb1b6e359679
klarstrup <io@klarstrup.dk>
2026-07-15 14:28:57 +0200

    feat: add additional options to the select dropdown in PageProductsItem component

--- ui/PageProductsItem.tsx
+++ ui/PageProductsItem.tsx
@@ -592,6 +592,12 @@ export default function PageProductsItem({
               <option value="4">4</option>
               <option value="5">5</option>
               <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
             </select>
           </Label>
         ) : null}
